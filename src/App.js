6ac58ddab9340211ed82b8896fd0da1902df57c3
Hassan Jamal <hassan@Hassan-MacBook-Pro.local>
2021-12-31 18:55:51 +0500

    Working on Memo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 import './App.css';
 // import Profile from './Components/Dashboard';
 // import StateTutorial from './Components/Dashboard/usestate';
-import EffectTutorial from './Components/Dashboard/useeffect';
+// import EffectTutorial from './Components/Dashboard/useeffect';
+import MemoTutorial from './Components/Dashboard/useMemo';
 
 
 function App() {
   return (
-
+    
 
           // <Profile text='Function Component Testing'/>
           // <StateTutorial />
-          <EffectTutorial />
+          <MemoTutorial />
   );
 }
 
